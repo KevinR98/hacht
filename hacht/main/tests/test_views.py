e273e05ab4595b969fe50058bb6eaf89c6e04813
Kevin <kevin.rojas277@outlook.es>
2020-07-22 21:48:01 -0600

    Comentarios y correccion de una prueba.

--- hacht/main/tests/test_views.py
+++ hacht/main/tests/test_views.py
@@ -9,7 +9,8 @@ from django.conf import settings as s
 from ..Analytics import Sorters
 from ..forms import RegistrationForm, Data_PacienteN
 from ..models import Paciente, Sesion, Muestra
-from ..views import login_app, index, ayuda, handle_error, crear_csv_demo
+from ..views import login_app, index, ayuda, handle_error, crear_csv_demo, handle_500_error
+
 
 #from ..Clients.Implementations import Web
 
@@ -49,31 +50,35 @@ class WebTestCase(TestCase):
         medico.delete()
         investigador.delete()
 
-    def test_loginapp_medico(self):
+    # Verifica el rotorno get del metodo loginapp con un usuario de rol medico logueado.
+    def test_loginapp_medico_get(self):
         self.client.login(username='medico', password='12345')
 
         response = self.client.get('/login_app/')
         self.assertEquals(response.status_code, 302)
         self.assertRedirects(response, '/dashboard_pacientes/')
 
-    def test_loginapp_investigador(self):
+    # Verifica el rotorno get del metodo loginapp con un usuario de rol investigador logueado.
+    def test_loginapp_investigador_get(self):
         self.client.login(username='investigador', password='12345')
 
         response = self.client.get('/login_app/')
         self.assertEquals(response.status_code, 302)
         self.assertRedirects(response, '/dashboard_sesiones/')
 
-
-    def test_about_us(self):
+    # Verifica el rotorno get del metodo about_us.
+    def test_about_us_get(self):
         response = self.client.get('/about_us/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/about_us.html')
 
-    def test_ayuda(self):
+    # Verifica el rotorno get del metodo ayuda.
+    def test_ayuda_get(self):
         response = self.client.get('/help/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/help.html')
 
+    # Verifica el rotorno del metodo handle_error.
     def test_handle_error(self):
         request = self.factory.get('')
         request.user = AnonymousUser()
@@ -81,22 +86,26 @@ class WebTestCase(TestCase):
         response = handle_error(request, 404, "Mensaje de error.")
         self.assertEquals(response.status_code, 404)
 
+    # Verifica el rotorno del metodo handle_500_error.
     def test_handle_500_error(self):
         request = self.factory.get('')
 
-        response = handle_error(request, 500, "Mensaje de error.")
+        response = handle_500_error(request, 500, "Mensaje de error.")
         self.assertEquals(response.status_code, 500)
 
+    # Verifica el rotorno get del metodo index.
     def test_index_get(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/index.html')
 
+    # Verifica el rotorno get del metodo registration.
     def test_registration_get(self):
         response = self.client.get('/registration/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/registration.html')
 
+    # Verifica el rotorno de error del metodo registration cuando un usuario esta iniciado.
     def test_registration_get_error_con_usuario_iniciado(self):
         self.client.login(username='medico', password='12345')
 
@@ -104,6 +113,7 @@ class WebTestCase(TestCase):
         self.assertEquals(response.status_code, 400)
         self.assertTemplateUsed(response, 'index/error.html')
 
+    # Verifica el rotorno post de registration, con un nuevo usuario por context.
     def test_registration_post(self):
 
         data = {
@@ -122,13 +132,14 @@ class WebTestCase(TestCase):
         usuario_creado = User.objects.get(username='prueba@email.com')
         usuario_creado.delete()
 
-
+    # Verifica el rotorno get del metodo demo.
     def test_demo_get(self):
         response = self.client.get('/demo/')
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/demo.html')
 
+    # Verifica el rotorno get del metodo demo con el indice de la imagen.
     def test_demo_get_abrir_comp_demo_con_imagen(self):
         response = self.client.get('/demo/components/comp_demo/', {'index': 0})
 
@@ -136,7 +147,7 @@ class WebTestCase(TestCase):
         self.assertTemplateUsed(response, 'index/components/comp_demo.html')
         self.assertEquals(0, response.context['index'])
 
-
+    # Verifica el rotorno get del metodo demo con el indice de una imagen y el resultado de esta.
     def test_demo_get_con_resultado(self):
         response = self.client.get('/demo/components/comp_demo/', {'index': 0, 'resultado': 0})
 
@@ -145,7 +156,7 @@ class WebTestCase(TestCase):
         self.assertEquals(0, response.context['index'])
         self.assertEquals('Adenosis', response.context['resultado'])
 
-
+    # Verifica el rotorno post del metodo demo con la informacion de una imagen por contexto.
     def test_demo_post_resultado_img(self):
         data = {
             'index': 0,
@@ -158,7 +169,7 @@ class WebTestCase(TestCase):
         self.assertTemplateUsed(response, 'index/demo.html')
         self.assertTrue('resultado' in response.context)
 
-
+    # Verifica el rotorno get del metodo dasboard_pacientes con un usuario medico logueado.
     def test_dashboard_pacientes_get_medico(self):
         self.client.login(username='medico', password='12345')
 
@@ -166,7 +177,7 @@ class WebTestCase(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/dashboard_pacientes.html')
 
-
+    # Verifica el rotorno get del metodo dasboard_pacientes con un usuario investigador logueado.
     def test_dashboard_pacientes_get_investigador(self):
         self.client.login(username='investigador', password='12345')
 
@@ -174,7 +185,7 @@ class WebTestCase(TestCase):
         self.assertEquals(response.status_code, 401)
         self.assertTemplateUsed(response, 'index/error.html')
 
-
+    # Verifica el error de rotorno post con mala informacion de un nuevo paciente del metodo dasboard_pacientes con un usuario medico logueado.
     def test_dashboard_pacientes_post_error(self):
         self.client.login(username='medico', password='12345')
         data = {
@@ -188,13 +199,13 @@ class WebTestCase(TestCase):
         self.assertEquals(response.status_code, 400)
         self.assertTemplateUsed(response, 'index/error.html')
 
-
+    # Verifica el rotorno get del metodo descriptivo_paciente.
     def test_descriptivo_paciente_get_form_vacio(self):
         response = self.client.get('/dashboard_pacientes/components/descriptivo_paciente/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/components/descriptivo_paciente.html')
 
-
+    # Verifica el rotorno get del metodo eliminar_paciente, prueba que se elimina.
     def test_eliminar_paciente(self):
         self.client.login(username='investigador', password='12345')
         response = self.client.get('/')
@@ -214,7 +225,7 @@ class WebTestCase(TestCase):
         lista = sorted(lista, key=Sorters.pacientes_sort_key, reverse=True)
         self.assertEquals(0, len(lista))
 
-
+    # Verifica el rotorno get del metodo dashboard_sesiones, revisa que retorne el paciente.
     def test_dashboard_sesiones_get_medico(self):
         self.client.login(username='medico', password='12345')
         response = self.client.get('/')
@@ -231,7 +242,7 @@ class WebTestCase(TestCase):
         self.assertTemplateUsed(response, 'index/dashboard_sesiones.html')
         self.assertEquals(paciente.id, response.context['paciente'].id)
 
-
+    # Verifica el rotorno get del metodo dashboard_sesiones.
     def test_dashboard_sesiones_get_investigador(self):
         self.client.login(username='investigador', password='12345')
 
@@ -239,11 +250,13 @@ class WebTestCase(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/dashboard_sesiones.html')
 
+    # Verifica el rotorno get del metodo dashboard_sesiones cuando no hay un usuario iniciado.
     def test_dashboard_sesiones_no_sesion(self):
         response = self.client.get('/dashboard_sesiones/')
         self.assertEquals(response.status_code, 401)
         self.assertTemplateUsed(response, 'index/error.html')
 
+    # Verifica el rotorno get del metodo descriptivo_sesion.
     def test_descriptivo_sesion(self):
         self.client.login(username='medico', password='12345')
         response = self.client.get('/')
@@ -263,7 +276,7 @@ class WebTestCase(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/components/descriptivo_sesion.html')
 
-
+    # Verifica el rotorno post del metodo eliminar_sesion.
     def test_eliminar_sesion(self):
         self.client.login(username='medico', password='12345')
         response = self.client.get('/')
@@ -283,7 +296,7 @@ class WebTestCase(TestCase):
         self.assertEquals(response.status_code, 204)
         self.assertTemplateUsed(response, 'index/error.html')
 
-
+    # Verifica el rotorno get del metodo muestras_sesion.
     def test_muestras_sesion(self):
         self.client.login(username='medico', password='12345')
         response = self.client.get('/')
@@ -303,7 +316,7 @@ class WebTestCase(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/components/muestras_sesion.html')
 
-
+    # Verifica el rotorno post del metodo agregar_muestra sin un un conjunto de archivos.
     def test_agregar_muestra_no_archivo_investigador(self):
 
         self.client.login(username='investigador', password='12345')
@@ -324,6 +337,7 @@ class WebTestCase(TestCase):
         self.assertEquals(response.status_code, 302)
         self.assertRedirects(response, '/dashboard_sesiones/')
 
+    # Verifica el rotorno post del metodo modificar_muestra, prueba que la muestra fuera eliminada.
     def test_modificar_muestra_delete(self):
 
         self.client.login(username='medico', password='12345')
@@ -355,16 +369,19 @@ class WebTestCase(TestCase):
         except Muestra.DoesNotExist:
             self.assertTrue(True)
 
+    # Verifica el rotorno get del metodo contact_us.
     def test_contact_us_get(self):
         response = self.client.get('/contact_us/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/contact-us.html')
 
+    # Verifica el rotorno get del metodo features.
     def test_features_get(self):
         response = self.client.get('/features/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index/features.html')
 
+    # Verifica el funcionamiento correcto del metodo crear_csv.
     def test_crear_csv_demo(self):
         try:
             crear_csv_demo()
